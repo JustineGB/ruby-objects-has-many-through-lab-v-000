6d47426ab8dfe127e3e0b2c9bcd0af109add4ba1
Justine Bricknell <justinegayton@gmail.com>
2018-07-28 21:58:29 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,10 +14,9 @@ class Patient
     @@all
   end
 
-  def new_appointment(doctor, date)
+  def new_appointment(doctor, date) #this instance meth is taking in 2 arguments (doctor and date which are attaccess in the appt class and thus they are an instance of that class and can acess that classes methods and the doctor class methods?)
     #binding.pry
-    Appointment.new(doctor, date, self )
-    appointments << self
+    Appointment.new(doctor, date, self ) #creating a new instance of Appointment (passing in the arguments, self as one) Should know about this instance know since I called self??.
   end
 
   def doctors
